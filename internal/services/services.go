0db05739f4643eb10e74224898f317def565eced
David Nakolan <david.nakolan@gmail.com>
2025-05-26 11:35:41 -0700

    Added a proximity check to service CreateTrail to prevent duplicates
    I noticed while testing that the POST /trails endpoint would accept and create
    multiple copies of the same trail by name. This is a consequence of me using
    trail_id (uuid) as the key to my data store rather than trail name. I chose to
    do this intentionally because there could be trails in different locations with
    exactly the same name (as I discovered while looking for examples of actual trails
    to use for testing). Rather than changing the key to the data store I instead chose
    to implement a 25km radius check for any trails with exactly the same name before
    creating a new trail. 25km is arbitrary and this adds some overhead to trail creation,
    especially as the data store scales, but it seemed like a fair compromise for this
    small project.

--- internal/services/services.go
+++ internal/services/services.go
@@ -8,6 +8,10 @@ import (
 	"github.com/dnakolan/trail-data-service/internal/storage"
 )
 
+const (
+	DUPLICATE_TRAIL_RADIUS_KM = 25.0
+)
+
 type TrailsService interface {
 	CreateTrail(ctx context.Context, trail *models.Trail) error
 	GetTrail(ctx context.Context, uid string) (*models.Trail, error)
@@ -26,6 +30,20 @@ func NewTrailsService(storage storage.TrailStorage) *trailsService {
 }
 
 func (s *trailsService) CreateTrail(ctx context.Context, trail *models.Trail) error {
+	radiusKm := DUPLICATE_TRAIL_RADIUS_KM
+	filter := &models.TrailFilter{
+		CreateTrailRequest: models.CreateTrailRequest{
+			Name: trail.Name,
+			Lat:  trail.Lat,
+			Lon:  trail.Lon,
+		},
+		RadiusKm: &radiusKm,
+	}
+
+	if duplicates, _ := s.GetAllTrails(ctx, filter); len(duplicates) > 0 {
+		return errors.New("trail already exists")
+	}
+
 	return s.storage.Save(ctx, trail)
 }
 
@@ -46,5 +64,6 @@ func (s *trailsService) GetAllTrails(ctx context.Context, filter *models.TrailFi
 }
 
 func (s *trailsService) GetNearbyTrails(ctx context.Context, lat float64, lon float64, radiusKm float64) ([]*models.Trail, error) {
+
 	return nil, errors.New("not implemented")
 }
